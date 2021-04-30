d143a0efdf29d09243f0bde8c487df7198c236f6
ayoubVD <ayoub.disneyti@gmail.com>
2021-04-30 14:28:22 +0200

    updated

---
+++ login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- 
+        <title>Counter Pick</title>
+    -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--
+        Pas de naam aan van JS
+        <script src="index.js"></script>
+    -->
+    <!--
+        Pas de naam aan van CSS
+        <link rel="stylesheet" type="text/css" href="/css/index.css">
+    -->
+    <link rel="stylesheet" type="text/css" href="/css/general.css" >
+    
+</head>
+<body>
+    <div id="container">
+        <header><ul><li>Home</li><li>Play</li><li>Contact</li></ul></header>
+        <span>Dit is span</span>
+        <p>Dit is een paragraaf</p>
+        
+        <?php include_once "./footer.php" ?>
+    </div>
+</body>
+</html>
