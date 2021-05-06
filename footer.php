aace606fad6bc6ed671ee176dcf3a83bf8260dcb
ayoubVD <ayoub.disneyti@gmail.com>
2021-05-06 10:32:20 +0200

    commit

--- footer.php
+++ footer.php
@@ -1,3 +1,6 @@
 <footer>
+    <a href="#">About us</a>
+    <a href="#">Teaser</a>
+    <br>
     © 2021 - Counterpick - Thomas More
 </footer>
--- index.php
+++ index.php
@@ -10,25 +10,24 @@
         Pas de naam aan van JS
         <script src="index.js"></script>
     -->
-    <!--
-        Pas de naam aan van CSS
-        <link rel="stylesheet" type="text/css" href="/css/index.css">
-    -->
+    <link rel="stylesheet" type="text/css" href="/css/index.css">
     <link rel="stylesheet" type="text/css" href="/css/general.css" >
     
 </head>
 <body>
     <div id="container">
-        <header><ul><li>Recruit</li><li>Play</li><li>Contact</li><li>Login</li></ul></header>
-        <h1>What is counterpick?</h1>
+        <h1 id="title">What is counterpick?</h1>
+        <header><ul><li>Play</li><li>Recruit</li><li><a href="contact.php">Contact</a></li><li><a href="login.php">Login</a>
+        <a href="login.php">Register</a></li></ul></header>
+        <div id="content">
         <p>
             Counterpick is an web application for everyone that is interested in playing League of Legends (LoL) at amateur level.<br>
             It will consist of teams of 5 players with relatively the same skillset. So you won’t be bullied by players with a much higher ranking.<br>
             All you would need is the LoL application, our web application and a stable network connection.<br>
             Register or log in now to start playing!<br>
         </p>
-        <a href="#">Login</a>
-        <a href="#">Register</a>
+        </div>
+        
         <?php include_once "./footer.php" ?>
     </div>
 </body>
