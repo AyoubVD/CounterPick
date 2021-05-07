81642350afd75a72bf523bda05a400d39fd67f02
David Wong <Davidwong99@hotmail.be>
2021-05-07 16:51:12 +0200

    oke

--- index.php
+++ index.php
@@ -5,14 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" type="icon" href="./img/logo.png">
     <!--
         Pas de naam aan van JS
         <script src="index.js"></script>
     -->
     <link rel="stylesheet" type="text/css" href="./css/index.css">
     <link rel="stylesheet" type="text/css" href="./css/general.css" >
-    <link rel="shortcut icon" href="http://sstatic.net/stackoverflow/img/favicon.ico">
 </head>
 <body>
     <div id="container">
