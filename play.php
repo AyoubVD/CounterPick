e42db00f314b258a8efc492b6fe9a5a79b3899e4
ayoubVD <ayoub.disneyti@gmail.com>
2021-05-07 13:25:05 +0200

    update css

--- play.php
+++ play.php
@@ -1,16 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Counter Pick</title>
+    <title>Play</title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="icon" href="./img/logo.png">
-    <!--
         Pas de naam aan van JS
-        <script src="index.js"></script>
-    -->
-    <link rel="stylesheet" type="text/css" href="/css/index.css">
+        <script src="recruit.js"></script>
+    <link rel="stylesheet" type="text/css" href="/css/recruit.css">
     <link rel="stylesheet" type="text/css" href="/css/general.css" >
     
 </head>
@@ -20,12 +18,8 @@
         <header>
             <a href="index.php"><img id="logo" src="img/logo.png"></a>
             <ul>
-                <li>
-                    <a href="recruit.php" >Recruit</a>
-                </li>
-                <li>
-                    <a href="play.php">Play</a>
-                </li>
+                <li>Recruit</li>
+                <li>Play</li>
                 <li>
                     <a href="contact.php">Contact</a>
                 </li>
