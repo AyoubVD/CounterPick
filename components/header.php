aa88b949199eb97bc4bd048a0c1ce8015618e05f
ayoubVD <ayoub.disneyti@gmail.com>
2021-05-07 14:13:12 +0200

    ha

--- components/header.php
+++ components/header.php
@@ -13,11 +13,5 @@
         <li>
             <a href="login.php">Login</a>
         </li>
-        <li>
-            <a href="login.php">Register</a>
-        </li>
-        <li>
-            <a href="signup.php">Sign up</a>
-        </li>
     </ul>
 </header>
