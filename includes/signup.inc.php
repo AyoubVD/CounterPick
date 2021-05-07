3669a8d9545d707af35df834d629571a03d09c5d
David Wong <Davidwong99@hotmail.be>
2021-05-07 17:43:12 +0200

    oke

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,6 +1,14 @@
 <?php
 if (isset($_POST["submit"])) {
-    echo "It works";
+
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $username = $_POST["uid"];
+    $pwd = $_POST["pwd"];
+    $repeatpwd = $_POST["repeatpwd"];
+    require_once 'dbh.inc.php';
+    require_once 'functions.inc.php';
+
 }
 else{
     header("location:../signup.php");
