c80b917ce08d47244459b1e9aef9d3bda92a5f99
David Wong <Davidwong99@hotmail.be>
2021-05-07 13:46:18 +0200

    oke

--- login.php
+++ login.php
@@ -7,6 +7,8 @@
     <h2>Log In</h2>
     <form action="login.inc.php" method="post">
      <input type="text" name="name" placeholder="Username/Email">
+     <br>
+     <br>
      <input type="password" name="pwd" placeholder="password">
      <button type="submit" name="submit">Log In</button>
     </form>
