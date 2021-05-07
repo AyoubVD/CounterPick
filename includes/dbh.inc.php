3e7ed0eca401e1626840409f4e38f540154ba393
David Wong <Davidwong99@hotmail.be>
2021-05-07 22:32:23 +0200

    oke

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,55 +1,15 @@
 <?php 
-    function connectToDb() {
-        $host = "ID328593_counterpick.db.webhosting.be";
-        $user = "ID328593_counterpick";
-        $password = "counterPick123";
-        $database = "ID328593_counterpick";
-        
-        $conn = mysqli_connect($host, $user, $password, $database);
-        if ($conn == false) die("Failed to connect to database!");
-        return $conn;
-    }
-
-
-    function closeConnection($conn) {
-        $conn->close();
-    }
-
-    function getQuery($sql) {
-        $conn = connectToDb();
-        $result = mysqli_query($conn,$sql);
-        closeConnection($conn);
-        return $result->fetch_all(MYSQLI_ASSOC);
-    }
-
-    function createQuery($sql) {
-        $conn = connectToDb();
-        $result = mysqli_query($conn,$sql);
-        closeConnection($conn);
-        return $result;
-    }
-
-    function updateQuery($sql) {
-        $conn = connectToDb();
-        $result = mysqli_query($conn,$sql);
-        closeConnection($conn);
-        return $result;
-    }
-
 
 $servername = "ID328593_counterpick.db.webhosting.be";
 $username = "ID328593_counterpick";
 $password = "counterPick123";
-$dbname = "ID328593_counterpickr";
+$dbname = "ID328593_counterpick";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if ($conn->connect_error) 
-{
-  die("Connection failed: " . $conn->connect_error);
-}
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
 
-return $conn;
 
-  
