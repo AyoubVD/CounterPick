704752ac85f5ca60e5f78a61fd0d0848dd0dd5ee
David Wong <Davidwong99@hotmail.be>
2021-05-08 21:31:24 +0200

    oke

--- create-new-password.php
+++ create-new-password.php
@@ -1,6 +1,6 @@
 <?php include_once "./components/header.php" ?>
 
-<section class="signup-form">
+<section class="section-default">
                 <h2>Reset your password</h2>
                <p>An e-mail will be send to you with instructions on how to reset your password.</p>
                <form action="./includes/reset-request.inc.php" method="post">
