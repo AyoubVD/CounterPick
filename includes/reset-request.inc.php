aa17fb2ab4b08b8c653fc42bb9a9abc3a9b706d0
David Wong <Davidwong99@hotmail.be>
2021-05-08 21:00:42 +0200

    oke

--- includes/reset-request.inc.php
+++ includes/reset-request.inc.php
@@ -5,7 +5,48 @@ if (isset($_POST["reset-request-submit"])) {
     $selector = bin2hex(random_bytes(8));
     $token = random_bytes(32);
 
-    $url = "https://counterpick.helpimstuckatschool.be/forgottenpwd/create-new-password.php?"
+    $url = "https://counterpick.helpimstuckatschool.be/forgottenpwd/create-new-password.php?selector=" . $selector."&validator=" . bin2hex($token);
+
+    $expires = date("U") + 1800;
+
+    require 'dbh.inc.php';
+
+    $userEmail = $_POST["email"];
+
+    $sql = "DELETE FROM pwdReset WHERE pwdResetEmail=?";
+
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt,$sql)) {
+        echo "There was error!"
+        exit();
+    }
+    else
+    {
+        mysqli_stmt_bind_param($stmt,"s",$userEmail);
+        mysqli_stmt_execute($stmt);
+    }
+
+    $sql = "INSERT INTO pwdReset(pwdResetEmail,pwdResetSelector,pwdResetToken,pwdResetExpires) VALUES(?,?,?,?);";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt,$sql)) {
+        echo "There was error!"
+        exit();
+    }
+    else
+    {
+        $hashedToken = password_hash($token,PASSWORD_DEFAULT);
+        mysqli_stmt_bind_param($stmt,"ssss",$userEmail,$selector,$hashedToken,$expires);
+        mysqli_stmt_execute($stmt);
+    }
+    mysqli_stmt_close($stmt);
+    mysqli_close();
+
+
+    $to = $userEmail;
+
+    $subject = 'Reset your password'
+
+    $message = '<p>';
 }
 else{
     header("location:../index.php");
