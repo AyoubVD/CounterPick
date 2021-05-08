1da612f976a9941b871b2ff115d52a0d50be108d
David Wong <Davidwong99@hotmail.be>
2021-05-08 23:05:52 +0200

    oke

--- includes/reset-request.inc.php
+++ includes/reset-request.inc.php
@@ -5,7 +5,7 @@ if (isset($_POST["reset-request-submit"])) {
     $selector = bin2hex(random_bytes(8));
     $token = random_bytes(32);
 
-    $url = "https://counterpick.helpimstuckatschool.be/forgottenpwd/create-new-password.php?selector=" . $selector."&validator=" . bin2hex($token);
+    $url = "https://counterpick.helpimstuckatschool.be/create-new-password.php?selector=" . $selector."&validator=" . bin2hex($token);
 
     $expires = date("U") + 1800;
 
