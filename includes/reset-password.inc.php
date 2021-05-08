f462b50dfc1647690b4f656a78e3d2d212473d2b
David Wong <Davidwong99@hotmail.be>
2021-05-08 23:24:53 +0200

    oke

--- includes/reset-password.inc.php
+++ includes/reset-password.inc.php
@@ -4,103 +4,103 @@ if (isset($_POST["reset-password-submit"])) {
     
 
 
- $selector = $_POST["selector"];
- $validator = $_POST["validator"];
- $Password = $_POST["pwds"];
- $passwordRepeat = $_POST["pwd-repeat"];
+    $selector = $_POST["selector"];
+    $validator = $_POST["validator"];
+    $Password = $_POST["pwds"];
+    $passwordRepeat = $_POST["pwd-repeat"];
 
- if (empty($Password) || empty($passwordRepeat)) {
-    header("location:../signup.php?newpwd=empty");
-    exit();
- }
- else if ($Password != $passwordRepeat) {
-    header("location:../signup.php?newpwd=pwdnotsame");
-    exit();
- }
+    if (empty($Password) || empty($passwordRepeat)) {
+        header("location:../signup.php?newpwd=empty");
+        exit();
+    }
+    else if ($Password != $passwordRepeat) {
+        header("location:../signup.php?newpwd=pwdnotsame");
+        exit();
+    }
 
- $currentDate = date("U");
+    $currentDate = date("U");
 
- require 'dbh.inc.php';
+    require 'dbh.inc.php';
 
- $sql = "SELECT* FROM pwdReset WHERE pwdResetSelector=? AND pwdResetExpires >= ?";
- $stmt = mysqli_stmt_init($conn);
- if (!mysqli_stmt_prepare($stmt,$sql)) {
-    echo "There was error!";
-    exit();
-}
-else
-{
-    mysqli_stmt_bind_param($stmt,"s",$selector);
-    mysqli_stmt_execute($stmt);
-
-    $result = mysqli_stmt_get_result($stmt);
-    if (!$row = mysqli_fetch_assoc($result)) {
-       echo "You need to re-submit your reset request.";
-       exit();
+    $sql = "SELECT* FROM pwdReset WHERE pwdResetSelector=? AND pwdResetExpires >= ?";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt,$sql)) {
+        echo "There was error!";
+        exit();
     }
     else
     {
-        $tokenBin = hex2bin($validator);
-        $tokenCheck = password_verify($tokenBin,$row["pwdResetToken"]);
+        mysqli_stmt_bind_param($stmt,"s",$selector);
+        mysqli_stmt_execute($stmt);
 
-
-        if ($tokenCheck == false) {
-            echo "You need to re-submit your reset request.";
-       exit();
+        $result = mysqli_stmt_get_result($stmt);
+        if (!$row = mysqli_fetch_assoc($result)) {
+        echo "You need to re-submit your reset request.";
+        exit();
         }
-        elseif ($tokenCheck == true) {
-            $tokenEmail = $row['pwdResetEmail'];
+        else
+        {
+            $tokenBin = hex2bin($validator);
+            $tokenCheck = password_verify($tokenBin,$row["pwdResetToken"]);
 
 
-            $sql = "SELECT * FROM users WHERE emailUseres=?;";
-            $stmt = mysqli_stmt_init($conn);
-            if (!mysqli_stmt_prepare($stmt,$sql)) {
-                echo "There was error!";
-                exit();
+            if ($tokenCheck == false) {
+                echo "You need to re-submit your reset request.";
+        exit();
             }
-            else
-            {
-                mysqli_stmt_bind_param($stmt,"s",$tokenEmail);
-                mysqli_stmt_execute($stmt);
-                if (!$row = mysqli_fetch_assoc($result)) {
-                    echo "There was an error!";
-                    exit();
-                 }
-                 else
-                 {
-                    $sql ="UPDATE users SET usersPwd=? WHERE usersEmail=?";
-                    $stmt = mysqli_stmt_init($conn);
-                    if (!mysqli_stmt_prepare($stmt,$sql)) {
+            elseif ($tokenCheck == true) {
+                $tokenEmail = $row['pwdResetEmail'];
+
+
+                $sql = "SELECT * FROM users WHERE emailUseres=?;";
+                $stmt = mysqli_stmt_init($conn);
+                if (!mysqli_stmt_prepare($stmt,$sql)) {
                     echo "There was error!";
                     exit();
-                    }
-                    else
-                    {
-                    $newPwdHash = password_hash($Password,PASSWORD_DEFAULT);
-                    mysqli_stmt_bind_param($stmt,"ss",$newPwdHash,$tokenEmail);
+                }
+                else
+                {
+                    mysqli_stmt_bind_param($stmt,"s",$tokenEmail);
                     mysqli_stmt_execute($stmt);
-
-                    $sql = "DELETE FROM pwdReset WHERE pwdResetEmail=?";
-                    $stmt = mysqli_stmt_init($conn);
-                    if (!mysqli_stmt_prepare($stmt,$sql)) {
-                        echo "There was error!";
+                    if (!$row = mysqli_fetch_assoc($result)) {
+                        echo "There was an error!";
                         exit();
                     }
                     else
                     {
-                        mysqli_stmt_bind_param($stmt,"s",$userEmail);
+                        $sql ="UPDATE users SET usersPwd=? WHERE usersEmail=?";
+                        $stmt = mysqli_stmt_init($conn);
+                        if (!mysqli_stmt_prepare($stmt,$sql)) {
+                        echo "There was error!";
+                        exit();
+                        }
+                        else
+                        {
+                        $newPwdHash = password_hash($Password,PASSWORD_DEFAULT);
+                        mysqli_stmt_bind_param($stmt,"ss",$newPwdHash,$tokenEmail);
                         mysqli_stmt_execute($stmt);
-                        header("location:../signup.php?newpwd=passwordupdated");
-                    }
+
+                        $sql = "DELETE FROM pwdReset WHERE pwdResetEmail=?";
+                        $stmt = mysqli_stmt_init($conn);
+                        if (!mysqli_stmt_prepare($stmt,$sql)) {
+                            echo "There was error!";
+                            exit();
+                        }
+                        else
+                        {
+                            mysqli_stmt_bind_param($stmt,"s",$userEmail);
+                            mysqli_stmt_execute($stmt);
+                            header("location:../signup.php?newpwd=passwordupdated");
+                        }
+
+                        }
 
                     }
-
-                 }
+                }
             }
-        }
 
+        }
     }
-}
 
 
 }
