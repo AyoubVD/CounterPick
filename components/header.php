4796be4daf9a0454bf6e33ae051c8e3332308566
David Wong <Davidwong99@hotmail.be>
2021-05-08 18:14:40 +0200

    oke

--- components/header.php
+++ components/header.php
@@ -17,7 +17,7 @@ error_reporting(0);
         </li>
       <?php
             if (isset($_SESSION["useruid"])) {
-                echo "<li><a href='login.php'>Profile page</a></li>";
+                echo "<li><a href='profile.php'>Profile page</a></li>";
                 echo "<li><a href='./includes/logout.inc.php'>Log out</a></li>";
             }
             else
