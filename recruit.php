0151530f81ce6660b265207ca83eb14cc97658a0
David Wong <Davidwong99@hotmail.be>
2021-05-08 20:13:15 +0200

    oke

--- recruit.php
+++ recruit.php
@@ -3,6 +3,7 @@
             <section class="signup-form">
                 <h2>Reset your password</h2>
                <p>An e-mail will be send to you with instructions on how to reset your password.</p>
+               <form action="./includes/reset-request.inc.php" method="post"></form>
             </section>
                 
         
