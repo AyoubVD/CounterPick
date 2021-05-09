86377920155f38210ccca4f3353af6cfd38ba1f6
David Wong <Davidwong99@hotmail.be>
2021-05-09 17:01:29 +0200

    oke

--- error_succeshandels.php
+++ error_succeshandels.php
@@ -1,39 +1,40 @@
 <?php include_once "./components/header.php" ?>
-        
+<div id="content">       
 <?php
 if (isset($_GET["error"])) {
    if ($_GET["error"] == "none") {
-        echo "<p>You have signed up!</p>";
+        echo "<p style='color:green;'>You have signed up!</p>";
     }
     if ($_GET["error"] == "emptypassword") {
-        echo "<p>Failed to create no input!</p>";
+        echo "<p style='color:red;>Failed to create no input!</p>";
     }
     if ($_GET["error"] == "passwordnotsame") {
-        echo "<p>Failed to create password not the same!</p>";
+        echo "<p style='color:red;'>Failed to create password not the same!</p>";
     }
 }
 
 if (isset($_GET["reset"])) {
     if($_GET["reset"] == "success"){
-        echo '<p class="signupsuccess">Check your e-mail!(check spam) </p>';
+        echo '<p style="color:green;">Check your e-mail!(check spam) </p>';
     }
 }
 if (isset($_GET["newpwd"])) {
     if ($_GET["newpwd"] == "passwordupdated") {
-        echo '<p class="signupsuccess">Your password has been reset!</p>';
+        echo '<p style="color:green;">Your password has been reset!</p>';
     }
 }
 if (isset($_GET["log"])) {
     if ($_GET["log"] == "out") {
-        echo '<p class="signupsuccess">You have been logged out!</p>';
+        echo '<p style="color:red;">You have been logged out!</p>';
     }
 }
 if (isset($_GET["log"])) {
     if ($_GET["log"] == "in") {
-        echo '<p class="signupsuccess">You have been logged in!</p>';
+        echo '<p style="color:green;">You have been logged in!</p>';
     }
 }
 ?>
+</div>
    </div>
         
         <?php include_once "./components/footer.php" ?>
