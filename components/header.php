69cdd98d72d20ccd22d931c4d86550c2b3abbea3
David Wong <Davidwong99@hotmail.be>
2021-05-09 12:34:18 +0200

    oke

--- components/header.php
+++ components/header.php
@@ -29,16 +29,18 @@
                 echo "<li><a href='profile.php'>Profile</a></li>";
                 echo "<li><a href='createteam.php'>Create Team</a></li>";
                 echo "<li><a href='./includes/logout.inc.php'>Log out</a></li>";
+                echo "<li><a href='contact.php'>Contact</a></li>";
             }
             else
             {
                 echo "<li><a href='login.php'>recruit</a></li>";
                 echo "<li><a href='play.php'>Play</a></li>";
                 echo "<li><a href='signup.php'>Sign up</a></li>";
-                echo "<li><a href='login.php'>Login</a></li>";                
+                echo "<li><a href='login.php'>Login</a></li>";
+                echo "<li><a href='contact.php'>Contact</a></li>";                
             }     
       ?>
-      <li><a href="contact.php">Contact</a></li>
+      
         
     </ul>
 </header>
