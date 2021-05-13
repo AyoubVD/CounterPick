bff20e9deb9f3fc866894c24f924ea48d3c933ff
David Wong <Davidwong99@hotmail.be>
2021-05-13 14:24:39 +0200

    oke

--- createteam.php
+++ createteam.php
@@ -1,9 +1,10 @@
 <?php include_once "./components/header.php"?>
 
+        <div id="content">
         <section class="signup-form">
             <h2>Create Team</h2>
-            <form action="./includes/createteam.inc.php" method="post">
-            <input type="file" id="img" name="img" accept="image/*">
+            <form action="./includes/createteam.inc.php" method="post" text-align: center;>
+            <input style="display:inline;text-align:center;" type="file" id="img" name="img" accept="image/*">
             <br>
             <br>
             <input type="text" name="name" placeholder="Team name">
@@ -12,7 +13,8 @@
             <button type="submit" name="submit">Create Team</button>
             </form>
         </section>
- <div>   
+        </div>
+        
         <?php include_once "./components/footer.php" ?>
     </div>
 </body>
