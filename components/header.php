84860bd81dc8271e3add17c4ab7800d6eafac41b
David Wong <Davidwong99@hotmail.be>
2021-05-13 17:31:45 +0200

    oke

--- components/header.php
+++ components/header.php
@@ -17,39 +17,33 @@
     <link rel="stylesheet" type="text/css" href="./css/general.css" >
     <link rel="shortcut icon" type="image/png" href="./img/logowhite.jpg">
 </head>
-<body>
-<div class="container-fluid" style="background-image: url('https://i.ytimg.com/vi/QzxSg4EfV60/maxresdefault.jpg');height:200px;">
-</head>
-</div>
-
-        
-<header>
-<nav class="navbar navbar-inverse navbar-static-top" data-spy="affix" data-offset-top="197">
-  
-    
-    <ul class="nav navbar-nav" style="padding-left: 10%;float:right;"> 
-    <a href="index.php"><img id="logo" src="img/logo.png"></a>
-      <?php    
-            if (isset($_SESSION["useruid"])) {
-                echo "<li><a href='recruit.php'>Recruit</a></li>";
-                echo "<li><a href='playafterlogin.php'>Play</a></li>";
-                echo "<li><a href='profile.php'>Profile</a></li>";
-                echo "<li><a href='createteam.php'>Create Team</a></li>";
-                echo "<li><a href='./includes/logout.inc.php'>Log out</a></li>";
-                echo "<li><a href='contact.php'>Contact</a></li>"; 
-            }
-            else
-            {
-                echo "<li><a href='login.php'>Recruit</a></li>";
-                echo "<li><a href='play.php'>Play</a></li>";
-                echo "<li><a href='signup.php'>Sign up</a></li>";
-                echo "<li><a href='login.php'>Login</a></li>";
-                echo "<li><a href='contact.php'>Contact</a></li>";              
-            }     
-      ?>
-    </ul>
-</nav>
-</header>
-<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<body>   
+    <div class="container-fluid" style="background-image: url('https://i.ytimg.com/vi/QzxSg4EfV60/maxresdefault.jpg');height:200px;"></div>      
+    <header>
+        <nav class="navbar navbar-inverse navbar-static-top" data-spy="affix" data-offset-top="197">
+            <ul class="nav navbar-nav" style="padding-left: 10%;float:right;"> 
+                <a href="index.php"><img id="logo" src="img/logo.png"></a>
+                <?php    
+                    if (isset($_SESSION["useruid"])) {
+                        echo "<li><a href='recruit.php'>Recruit</a></li>";
+                        echo "<li><a href='playafterlogin.php'>Play</a></li>";
+                        echo "<li><a href='profile.php'>Profile</a></li>";
+                        echo "<li><a href='createteam.php'>Create Team</a></li>";
+                        echo "<li><a href='./includes/logout.inc.php'>Log out</a></li>";
+                        echo "<li><a href='contact.php'>Contact</a></li>"; 
+                    }
+                    else
+                    {
+                        echo "<li><a href='login.php'>Recruit</a></li>";
+                        echo "<li><a href='play.php'>Play</a></li>";
+                        echo "<li><a href='signup.php'>Sign up</a></li>";
+                        echo "<li><a href='login.php'>Login</a></li>";
+                        echo "<li><a href='contact.php'>Contact</a></li>";              
+                    }           
+                ?>
+            </ul>
+        </nav>
+    </header>
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
