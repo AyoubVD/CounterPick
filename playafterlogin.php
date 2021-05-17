2d25600c785b753cf2759fda76dbf4964ff5d72a
David Wong <Davidwong99@hotmail.be>
2021-05-17 19:09:41 +0200

    oke

--- playafterlogin.php
+++ playafterlogin.php
@@ -1,5 +1,37 @@
 <?php include_once "./components/header.php"?>
     <p>play afterlogin works page</p>
+    <table class="table table-striped">
+    <thead>
+        <tr>
+            <th>UUID</th>
+            <th>Full Name</th>
+            <th>Email</th>
+            <th>Access Key</th>
+            <th>Phone Number</th>
+            <th>Activated</th>
+            <th>Role</th>
+        </tr>
+    </thead>
+    <tbody>
+
+    <?php
+include_once('./includes/dbh.inc.php');
+$records = mysqli_query($conn,"select * from users"); // fetch data from database
+
+while($data = mysqli_fetch_array($records))
+{
+?>
+  <tr>
+    <td><?php echo $data['usersId']; ?></td>
+    <td><?php echo $data['userName']; ?></td>
+    <td><?php echo $data['usersEmail']; ?></td>
+  </tr>	
+<?php
+}
+?>
+</table>
+    </tbody>
+</table>
 <?php include_once "./components/footer.php" ?>
 </body>
 </html>
