e6fe9feadb14dc597e03ddb5162dab25122b0412
David Wong <Davidwong99@hotmail.be>
2021-05-23 21:31:08 +0200

    oke

--- create_team/src/round-robin-functions.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-require_once 'functions/rotate.php';
-require_once 'functions/make_schedule.php';
--- src/round-robin.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once 'round-robin-functions.php';
-require_once 'objects/ScheduleBuilder.php';
-require_once 'objects/Schedule.php';
