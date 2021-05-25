2b770d45e9c946b9893ca2ca1675c29631304132
David Wong <Davidwong99@hotmail.be>
2021-05-25 21:22:30 +0200

    oke

---
+++ contact_login.php
@@ -0,0 +1,32 @@
+<?php include_once "./components/header.php" ?>
+    <div class="signup-form">
+        <form action="./includes/contact.inc.php" method="post">
+            First Name:<br> <input type="text" name="first_name">
+            <br>
+            <br>
+            Last Name:<br> <input type="text" name="last_name">
+            <br>
+            <br>
+            Email:<br><input type="text" name="email">
+            <br>
+            <br>
+            Message:<br><textarea rows="5" name="message" cols="30"></textarea>
+            <br>
+            <input type="submit" name="submit" value="Submit">
+        </form>
+        <?php
+            if (isset($_GET["error"])) {
+                if ($_GET["error"] == "emptyinput") {
+                    echo "<p style='color:red;'>Fill in all fields!</p>";
+                }
+        
+                else if ($_GET["error"] == "invalidemail") {
+                  echo "<p style='color:red;'>Not an email!</p>";
+                } 
+            }
+            ?>
+    </div>
+    <?php include_once "./components/footer.php" ?>
+</body>
+</html>
+
