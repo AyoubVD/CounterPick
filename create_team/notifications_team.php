6c95a6b6e1195ad683eee2a3178c6a707b0b8152
David Wong <Davidwong99@hotmail.be>
2021-05-27 15:28:31 +0200

    oke

--- create_team/notifications_team.php
+++ create_team/notifications_team.php
@@ -41,16 +41,34 @@ $get_all_req_sender = $frnd_obj->request_notification($_SESSION['user_id'], true
 ?>
     <div class="profile_container">
         
-        <div class="inner_profile">
+    <div class="inner_profile" style = "display: flex;
+                        flex-wrap: wrap;
+                        align-items: center;
+                        border: 1px solid rgba(23,23,23, .2);
+                        margin: 5px;
+                        padding: 50px;
+                        width: 20%;
+                        background-color: #FFF;
+                        align-items: stretch;
+                        display: flex;
+                        flex-wrap: wrap;
+                        justify-content: center;
+                        text-align: center;
+                        align-content: center;
+                        margin-left: auto;
+                        margin-right: auto;
+                        background-image: url(https://i.pinimg.com/736x/c1/7f/31/c17f318095085ff32bb7fadbb1f9a89a.jpg);color:white;">
             <div class="img">
-                <img src="profile_images/<?php echo $user_data->user_image; ?>" alt="Profile image">
+                <img width="48" height="48" src="profile_images/<?php echo $user_data->user_image; ?>" alt="Profile image">
+                <h1>team:<?php echo  $user_data->teamname;?></h1>
+            </div>
             </div>
             <h1>team:<?php echo  $user_data->teamname;?></h1>
             <h1>info:<?php echo  $user_data->bio;?></h1>
             <h1>looking:<?php echo  $user_data->looking;?></h1>
             <h1>win:<?php echo  $user_data->win;?></h1>
             <h1>loss:<?php echo  $user_data->loss;?></h1>
-        </div>
+        <br>
         <nav>
             <ul>
                 <li><a href="notifications_team.php" rel="noopener noreferrer" class="active">Requests<span class="badge <?php
@@ -59,6 +77,7 @@ $get_all_req_sender = $frnd_obj->request_notification($_SESSION['user_id'], true
                 }
                 ?>"><?php echo $get_req_num;?></span></a></li>
                 <li><a href="friends_team.php" rel="noopener noreferrer">Players<span class="badge"><?php echo $get_frnd_num;?></span></a></li>
+                <li><a href='settings.php'>Settings</a></li>
             </ul>
         </nav>
         <div class="all_users"> 
@@ -66,7 +85,24 @@ $get_all_req_sender = $frnd_obj->request_notification($_SESSION['user_id'], true
                 <?php
                 if($get_req_num > 0){
                     foreach($get_all_req_sender as $row){
-                        echo '<div class="user_box">
+                        echo '<div class="user_box" style = "display: flex;
+                        flex-wrap: wrap;
+                        align-items: center;
+                        border: 1px solid rgba(23,23,23, .2);
+                        margin: 5px;
+                        padding: 5px;
+                        width: 5%;
+                        background-color: #FFF;
+                        align-items: stretch;
+                        display: flex;
+                        flex-wrap: wrap;
+                        justify-content: center;
+                        text-align: center;
+                        align-content: center;
+                        margin-left: auto;
+                        margin-right: auto;
+                        background-image: url(https://i.pinimg.com/736x/c1/7f/31/c17f318095085ff32bb7fadbb1f9a89a.jpg);color:white;"
+                        ">
                                 <div class="user_img"><img src="profile_images/'.$row->user_image.'" alt="Profile image"></div>
                                 <h1>ign:<div class="user_info"><span>'.$row->username.'</span></h1>
                                 <p>role:<div class="user_info"><span>'.$row->role.'</span></p> 
--- create_team/profile_team.php
+++ create_team/profile_team.php
@@ -21,17 +21,34 @@ $get_frnd_num = $frnd_obj->get_all_friends($_SESSION['user_id'], false);
 <?php include_once "header.php" ?>
     <div class="profile_container">
 
-        <div class="inner_profile">
+        <div class="inner_profile" style = "display: flex;
+                        flex-wrap: wrap;
+                        align-items: center;
+                        border: 1px solid rgba(23,23,23, .2);
+                        margin: 5px;
+                        padding: 50px;
+                        width: 20%;
+                        background-color: #FFF;
+                        align-items: stretch;
+                        display: flex;
+                        flex-wrap: wrap;
+                        justify-content: center;
+                        text-align: center;
+                        align-content: center;
+                        margin-left: auto;
+                        margin-right: auto;
+                        background-image: url(https://i.pinimg.com/736x/c1/7f/31/c17f318095085ff32bb7fadbb1f9a89a.jpg);color:white;">
             <div class="img">
-                <img src="profile_images/<?php echo $user_data->user_image; ?>" alt="Profile image">
+                <img width="48" height="48" src="profile_images/<?php echo $user_data->user_image; ?>" alt="Profile image">
+                <h1>team:<?php echo  $user_data->teamname;?></h1>
+            </div>
             </div>
             <h1>team:<?php echo  $user_data->teamname;?></h1>
             <h1>info:<?php echo  $user_data->bio;?></h1>
             <h1>looking:<?php echo  $user_data->looking;?></h1>
             <h1>win:<?php echo  $user_data->win;?></h1>
             <h1>loss:<?php echo  $user_data->loss;?></h1>
-            
-        </div>
+        <br>
         <nav>
             <ul>
                 <li><a href="notifications_team.php" rel="noopener noreferrer">Requests<span class="badge <?php
