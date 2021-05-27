8bb4358dc4bf10437f87634eeb8063d9ad9df39b
David Wong <Davidwong99@hotmail.be>
2021-05-27 15:50:41 +0200

    oke

--- create_team/contact.php
+++ create_team/contact.php
@@ -19,20 +19,37 @@ $get_req_num = $frnd_obj->request_notification($_SESSION['user_id'], false);
 $get_frnd_num = $frnd_obj->get_all_friends($_SESSION['user_id'], false);
 ?>
 <?php include_once "header.php" ?>
-    <div class="signup-form">
+    <div class="signup-form" style = "display: flex;
+                        flex-wrap: wrap;
+                        align-items: center;
+                        border: 1px solid rgba(23,23,23, .2);
+                        margin: 5px;
+                        padding: 50px;
+                        width: 20%;
+                        background-color: #FFF;
+                        align-items: stretch;
+                        display: flex;
+                        flex-wrap: wrap;
+                        justify-content: center;
+                        text-align: center;
+                        align-content: center;
+                        margin-left: auto;
+                        margin-right: auto;
+                        background-image: url(https://images2.minutemediacdn.com/image/upload/c_fill,w_1200,h_630,f_auto,q_auto,g_auto/shape/cover/sport/5c2cd98abb4b87834e000001.jpeg);color:white;">
+        <h1>Contact us</h1>
         <form action="./includes/contact.inc.php" method="post">
-            First Name:<br> <input type="text" name="first_name">
+            First Name:<br> <input style="color:black;" type="text" name="first_name">
             <br>
             <br>
-            Last Name:<br> <input type="text" name="last_name">
+            Last Name:<br> <input style="color:black;" type="text" name="last_name">
             <br>
             <br>
-            Email:<br><input type="text" name="email">
+            Email:<br><input style="color:black;"  type="text" name="email">
             <br>
             <br>
-            Message:<br><textarea rows="5" name="message" cols="30"></textarea>
+            Message:<br><textarea style="color:black;"  rows="5" name="message" cols="30"></textarea>
             <br>
-            <input type="submit" name="submit" value="Submit">
+            <input style="color:black;" type="submit" name="submit" value="Submit">
         </form>
         <?php
             if (isset($_GET["error"])) {
@@ -49,4 +66,3 @@ $get_frnd_num = $frnd_obj->get_all_friends($_SESSION['user_id'], false);
     <?php include_once "footer.php" ?>
 </body>
 </html>
-
