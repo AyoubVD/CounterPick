fa2573ddc703428e6405182f03b30b0603d9a711
David Wong <Davidwong99@hotmail.be>
2021-05-27 16:02:22 +0200

    oke

--- create_team/chat.php
+++ create_team/chat.php
@@ -21,7 +21,7 @@ $get_frnd_num = $frnd_obj->get_all_friends($_SESSION['user_id'], false);
 <?php include_once "header.php" ?>
     
     <title>Chat</title>
-    <h1><?php echo  $user_data->teamname;?></h1>
+   
     
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
     <script type="text/javascript" src="chat.js"></script>
@@ -89,7 +89,9 @@ $get_frnd_num = $frnd_obj->get_all_friends($_SESSION['user_id'], false);
 
 </head>
 
-<body onload="setInterval('chat.update()', 1000)">
+<body onload="setInterval('chat.update()', 1000)" >
+
+<h1>Find teams to play against!</h1>
 
     <div id="page-wrap">
     
@@ -99,7 +101,24 @@ $get_frnd_num = $frnd_obj->get_all_friends($_SESSION['user_id'], false);
         
         <div id="chat-wrap"><div id="chat-area"></div></div>
         
-        <form id="send-message-area">
+        <form id="send-message-area"  style = "display: flex;
+                        flex-wrap: wrap;
+                        align-items: center;
+                        border: 1px solid rgba(23,23,23, .2);
+                        margin: 5px;
+                        padding: 5px;
+                        width: 20%;
+                        background-color: #FFF;
+                        align-items: stretch;
+                        display: flex;
+                        flex-wrap: wrap;
+                        justify-content: center;
+                        text-align: center;
+                        align-content: center;
+                        margin-left: auto;
+                        margin-right: auto;
+                        background-image: url(https://i.pinimg.com/736x/c1/7f/31/c17f318095085ff32bb7fadbb1f9a89a.jpg);"
+                        >
             <p>Your message: </p>
             <textarea id="sendie" maxlength = '100' ></textarea>
         </form>
