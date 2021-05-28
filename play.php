45462ab934bcf83354bc7cd63215103c3c7cd361
David Wong <Davidwong99@hotmail.be>
2021-05-28 13:26:41 +0200

    oke

--- play.php
+++ play.php
@@ -1,5 +1,5 @@
 <?php include_once "./components/header.php" ?>
-<div class="contain" style = "display: flex;
+<h1 style = "display: flex;
                         flex-wrap: wrap;
                         align-items: center;
                         border: 1px solid rgba(23,23,23, .2);
@@ -15,28 +15,58 @@
                         align-content: center;
                         margin-left: auto;
                         margin-right: auto;
-                        font-size:1.2vw;
-                        background-image: url(https://lolstatic-a.akamaihd.net/frontpage/apps/prod/clash-2018/en_GB/a46e742ae82f9d4f9db8e34ba57873e513e727b7/assets/static/img/backgrounds/brackets-bg.jpg);color:white;">
-        <h1>Intro</h1>
-        <p>
-            Counterpick is an web application for everyone that is interested in playing League of Legends (LoL) at amateur level.<br>
-            It will consist of teams of 5 players with relatively the same skillset. So you won’t be bullied by players with a much higher ranking.<br>
-            All you would need is the LoL application, our web application and a stable network connection.<br>
-            Register or log in now to start playing!<br>
-        </p> 
-        <h1>Cant wait?</h1>
-        <h2>Do not smurf please!</h2>
-        <h2>This is for beginners and for players from unranked to silver!</h2>
-        <h1><a style="color:white;" href='login_as.php' padding-left="2%">login here!</a></h1>
-        <h1><a style="color:white;" href='signup_as.php'>register here!</a></h1>
-        <br>
-        <footer style="color:white;">
+                        background-image: url('https://lolstatic-a.akamaihd.net/frontpage/apps/prod/clash-2018/en_GB/a46e742ae82f9d4f9db8e34ba57873e513e727b7/assets/static/img/backgrounds/brackets-bg.jpg');
+                        color:white;" >Welkom bij Counterpick!</h1>
+<br>
+<br>
+<br>
+        <div id="content" style = "display: flex;
+                        flex-wrap: wrap;
+                        align-items: center;
+                        border: 1px solid rgba(23,23,23, .2);
+                        margin: 5px;
+                        padding: 5px;
+                        width: 20%;
+                        background-color: #FFF;
+                        align-items: stretch;
+                        display: flex;
+                        flex-wrap: wrap;
+                        justify-content: center;
+                        text-align: center;
+                        align-content: center;
+                        margin-left: auto;
+                        margin-right: auto;
+                        background-image: url('https://lolstatic-a.akamaihd.net/frontpage/apps/prod/clash-2018/en_GB/a46e742ae82f9d4f9db8e34ba57873e513e727b7/assets/static/img/backgrounds/brackets-bg.jpg');
+                        color:white;">
+            
+            <h2>What is CounterPick?</h2>
+            <br>
+            <h2>Do not smurf please!</h2>
+            <h2>This is for beginners and for players from unranked to silver!</h2>
+            <h3>CounterPick is <u>four</u> points</h3>
+            <br>
+            <p>To help and guide anyone who wants to play competitively at the amateur level.</p>
+            <br>
+            <p>For everybody who plays competitively comp LOL and is an amateur.</p>
+            <br>
+            <p>A web application where you can find data on the current state of the tournament.</p>
+            <br>
+            <p>We are using an python script to get the necessary information on the players and using it to match the players on the same skillset.</p>
+            <br>
+            <h3>
+            So you might ask, isn't this a lot. <br>
+            For us it is, but for the players it isn't. <br>
+            We will list <b>all</b> the requirements below.
+            </h3>
+            <br>
+            <p>LOL installed, updated web browser and a stable network connection.test</p>      
+            <p>As you can see this isn't a lot, so what's holding you back? <br>
+            Come and join the community to become a better League player!</p>
+            <footer style="color:white;">
     <a style="color:white;" href="https://counterpick123.wordpress.com/">About us</a>
     <a style="color:white;" href="https://www.youtube.com/watch?v=aqdPN6tpFfo">Teaser</a>
     <br>
     © 2021 - Counterpick - Thomas More
 </footer>
-        </div>
- 
 </body>
 </html>
